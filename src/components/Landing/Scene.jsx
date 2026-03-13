0aa8b1d2aa0a05393876a6e8594cc6f6a0262808
saur0w <sthapliyal085@gmail.com>
2026-03-13 22:41:12 +0530

    "Final Commit"

--- src/components/Landing/Scene.jsx
+++ src/components/Landing/Scene.jsx
@@ -19,17 +19,12 @@ export default function Scene() {
 
     return (
         <>
-            <Environment preset="city" />
+            <Environment preset="sunset" />
             <ambientLight intensity={0.6} />
             <directionalLight position={[5, 10, 5]} intensity={1.5} castShadow />
 
             <PresentationControls
-                global
-                config={{ mass: 2, tension: 400 }}
-                snap={{ mass: 4, tension: 1500 }}
                 rotation={[0, 0.3, 0]}
-                polar={[-Math.PI / 4, Math.PI / 4]}
-                azimuth={[-Math.PI / 2, Math.PI / 2]}
             >
                 <Float speed={1.5} rotationIntensity={0.2} floatIntensity={0.4}>
                     <primitive
